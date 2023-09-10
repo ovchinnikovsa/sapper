7b188f9468f038576ef67c6a345ef4e503d93f26
Ovchie <ov4innikovsa@yandex.ru>
2023-09-10 14:39:30 +0300

    addClassesAndModifyDockerConf

--- src/index.php
+++ src/index.php
@@ -3,5 +3,10 @@
 declare(strict_types=1);
 session_start();
 
-require_once __DIR__ . '/modules/index.php';
-require_once __DIR__ . '/view/pages/index.php';
+require_once __DIR__ . '/../vendor/autoload.php';
+
+use Core\Session;
+
+
+Session::set('test', 1);
+var_dump(Session::get('test'));
